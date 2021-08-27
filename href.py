e6ae3483d09ef1cacedd982d1c548c9b32148bd5
shabane <m.mohamadshabane@gmail.com>
2021-08-27 16:44:04 +0430

    program work correctly

--- href.py
+++ href.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+
+def GetAllTags(url: str, pattern: str):
+    site = requests.get(url)
+    if(site.status_code != 200):
+        print('err:', site.status_code)
+    else:
+        site = bs(site.content, 'html.parser')
+        site = site.find_all('a')
+        for i in site:
+            if(re.search(f'{pattern}', i['href'])):
+                print(re.search(f'{pattern}', i['href']).string)
+
+
