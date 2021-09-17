68c862cc6e4b125b0a80031d3b516042f4436e4e
shabane <m.mohamadshabane@gmail.com>
2021-09-17 22:08:11 +0430

    try added to handle error

--- href.py
+++ href.py
@@ -9,25 +9,27 @@ import sys
 
 # creating CLI
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', type=str, required=True, help='the url of the page')
-parser.add_argument('-p', '--pattern', type=str, required=False, help='regex pattern(case sensitive)')
-parser.add_argument('-l', '--load-headers', help='load header from file')
+parser.add_argument("-u", "--url", type=str, required=True, help="the url of the page")
+parser.add_argument(
+    "-p", "--pattern", type=str, required=False, help="regex pattern(case sensitive)"
+)
+parser.add_argument("-l", "--load-headers", help="load header from file")
 
 args = parser.parse_args()
 
 url = args.url
 
 pattern = args.pattern
-if(pattern == None):
-    pattern = '.*'
+if pattern == None:
+    pattern = ".*"
 
-if(args.load_headers != None):
-    with open('headers', 'r') as fli:
+if args.load_headers != None:
+    with open("headers", "r") as fli:
         fli = json.load(fli)
-        if(type(fli) != dict):
-            print('err: the header file should be a json file', file=sys.stderr)
-        elif(type(fli) == dict):
-            headers=fli
+        if type(fli) != dict:
+            print("err: the header file should be a json file", file=sys.stderr)
+        elif type(fli) == dict:
+            headers = fli
 else:
     headers = {}
 
@@ -37,13 +39,17 @@ def GetAllTags(url: str, pattern: str):
 
     site = requests.get(url, headers=headers)
 
-    if(site.status_code != 200):
-        print('err:', site.status_code, file=sys.stderr)
+    if site.status_code != 200:
+        print("err:", site.status_code, file=sys.stderr)
     else:
-        site = bs(site.content, 'html.parser')
-        site = site.find_all('a')
+        site = bs(site.content, "html.parser")
+        site = site.find_all("a")
         for i in site:
-            if(re.search(f'{pattern}', i['href'], re.IGNORECASE)):
-                print(re.search(f'{pattern}', i['href'], re.IGNORECASE).string)
+            try:
+                if re.search(f"{pattern}", i["href"], re.IGNORECASE):
+                    print(re.search(f"{pattern}", i["href"], re.IGNORECASE).string)
+            except:
+                pass
+
 
 GetAllTags(url, pattern)
